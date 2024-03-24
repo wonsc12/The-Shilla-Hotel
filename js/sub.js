762f8ef9d93350ebee956d7509030123fd6f0557
wonsc12 <turnapple123@naver.com>
2024-03-25 04:11:07 +0900

    [new]최원석/서브페이지/html/css/jquery작업

--- js/sub.js
+++ js/sub.js
@@ -2,3 +2,49 @@ $('#file').change(function(){
 	let fileName = $(this).val().split('\\').pop(); // 파일 경로에서 파일명만 추출
 	$('.file-name').text(fileName).addClass('on'); // 파일명 표시
 });
+
+
+
+
+
+// 서브페이지 객실소개
+
+const swiper1 = new Swiper(".myswiper", {
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+		loop:true,
+		pauseOnMouseEnter:true
+	},	
+		slidesPerView: 1,
+		direction:"horizontal",
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable:true
+	},
+
+});
+
+// 서브페이지 다이닝
+
+const swiper2= new Swiper(".myswiper2", {
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+		loop:true,
+		pauseOnMouseEnter:true
+	},	
+		slidesPerView: 1,
+		direction:"horizontal",
+
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},	
+		pagination: {
+			el: '.swiper-pagination',
+			clickable:true
+		},
+
+});
