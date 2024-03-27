dcd01579481b9be0e673a958af791128bcb6e6cb
psh <sosohy38@gmail.com>
2024-03-27 18:18:10 +0900

    [mod]박소현/js/jquery변경

--- js/pub.js
+++ js/pub.js
@@ -38,5 +38,95 @@ $(function(){
 		};
 	})
 
+	// 파일 업로드 텍스트 출력
+	$('#file').change(function(){
+		let fileName = $(this).val().split('\\').pop(); // 파일 경로에서 파일명만 추출
+		$('.file-name').text(fileName).addClass('on'); // 파일명 표시	
+	});
 
+
+	// 데이트픽커 공통
+	// $( ".date-picker").each((index, item)=>{
+	// 	// console.log($(item).data('month'), $(item).data('month') != undefined )
+	// 	$(item).datepicker();
+
+	// 	//한글적용을 위해 추가
+	// 	$.datepicker.setDefaults({
+	// 		dateFormat: 'yy-mm-dd',
+	// 		prevText: '이전 달',
+	// 		nextText: '다음 달',
+	// 		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+	// 		monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+	// 		dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+	// 		dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+	// 		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+	// 		showMonthAfterYear: true,
+	// 		yearSuffix: '년'
+	// 		// defaultDate: '+1m'
+	// 		// defaultDate: $(item).data('month') != undefined ? +$(item).data('month')+'m' : '+1m'
+	// 		// defaultDate: $(item).data('month') != undefined ? '+5m' : '+1m'
+			
+	// 	});
+	// })
+
+	// 현재달
+	$("#today-month").datepicker({
+		dateFormat: 'yy-mm-dd',
+		prevText: '이전 달',
+		nextText: '다음 달',
+		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		showMonthAfterYear: true,
+		yearSuffix: '년',
+		
+	});
+
+	// 다음달
+	$("#next-month").datepicker({
+		dateFormat: 'yy-mm-dd',
+		prevText: '이전 달',
+		nextText: '다음 달',
+		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		showMonthAfterYear: true,
+		yearSuffix: '년',
+		defaultDate: '+1m'
+		
+	});
+
+
+	// FAQ TAB
+	$('.tab-list.faq li').eq(0).addClass('on');
+	$('.tab-list.faq .tab-pannel').eq(0).addClass('on');
+	
+	$('.tab-list.faq li').click(function(e){
+		e.preventDefault();
+		$('.tab-list.faq li').removeClass('on');
+		$(this).addClass('on');
+		
+		$(this).closest('.tab-wrap').find('.tab-content > .tab-pannel').removeClass('on');
+		$(this).closest('.tab-wrap').find('.tab-content > .tab-pannel:eq('+$(this).closest('li').index()+')').addClass('on');
+	})
+
+	// ACCORDIAN
+	$('.acco-item').eq(0).addClass('on');
+	
+	$('.acco-btn').click(function(e){
+		e.preventDefault();
+		console.log($(this).closest('.acco-item'))
+		if(!$(this).closest('.acco-item').hasClass('on')){
+			$(this).closest('.acco-item').addClass('on');
+			$(this).closest('.acco-head').siblings('.acco-body').slideDown(400);
+		}else{
+			$(this).closest('.acco-item').removeClass('on');
+			$(this).closest('.acco-head').siblings('.acco-body').slideUp(400);
+		}
+	})
+	
 });
