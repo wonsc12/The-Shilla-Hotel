a7b6646e2aac216575fd5b1ddf0062db84ada6da
LeeDaYoungss <leedayeonggg@gmail.com>
2024-03-30 19:30:55 +0900

    [mod]이다영/html+js/서울신라작업

--- js/sub_02.js
+++ js/sub_02.js
@@ -1,13 +1,32 @@
 $(function(){ 
 
 
-  let slideWrapper = $('.slide-wrapper'),
-  slideContainer = slideWrapper.find('.slide-container'),
+  let slideWrapper = $('.seoul-right-slideimg'),
+  slideContainer = slideWrapper.find('.seoul-right-slideimg ul'),
   slides = slideContainer.find('li'),
-  pager = slideWrapper.find('.pager'),
   prevBtn = slideWrapper.find('#prev'),
   nextBtn = slideWrapper.find('#next'),    
   currentIdx = 0;
+
+  //slideContainer의 너비 지정
+  slideWidth = slideWrapper.outerWidth();
+  slideContainer.css({width:slideWidth * slides.length+'px'});
+
+  //슬라이드 이동 함수
+  function moveSlide(num){
+    let moveImg = -num * slideWidth;
+    slideContainer.stop().animate({transform:`translateX(${moveImg}px)`});
+    currentIdx = num;
+    updateSlide();
+  }
+
+  nextBtn.click(()=>{    
+    moveSlide(currentIdx + 1);      
+  });
+
+  prevBtn.click(()=>{      
+    moveSlide(currentIdx - 1);
+  });
 
   
 });
