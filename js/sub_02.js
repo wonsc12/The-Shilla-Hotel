9d9934e38af54eda18c415af2cecf0da7d5f6b5a
LeeDaYoungss <leedayeonggg@gmail.com>
2024-03-30 21:29:46 +0900

    [new]이다영/html+css+js/서울호텔작업

--- js/sub_02.js
+++ js/sub_02.js
@@ -1,12 +1,12 @@
 $(function(){ 
 
 
-  let slideWrapper = $('.seoul-right-slideimg'),
-  slideContainer = slideWrapper.find('.seoul-right-slideimg ul'),
-  slides = slideContainer.find('li'),
-  prevBtn = slideWrapper.find('#prev'),
-  nextBtn = slideWrapper.find('#next'),    
-  currentIdx = 0;
+  let slideWrapper = $('.seoul-right-slide'),
+      slideContainer = slideWrapper.find('ul'),
+      slides = slideContainer.find('li'),
+      prevBtn = $('#prev'),
+      nextBtn = $('#next'),    
+      currentIdx = 0;
 
   //slideContainer의 너비 지정
   slideWidth = slideWrapper.outerWidth();
@@ -14,17 +14,20 @@ $(function(){
 
   //슬라이드 이동 함수
   function moveSlide(num){
+    console.log(num);
     let moveImg = -num * slideWidth;
     slideContainer.stop().animate({transform:`translateX(${moveImg}px)`});
     currentIdx = num;
-    updateSlide();
+    // updateSlide();
   }
 
-  nextBtn.click(()=>{    
+  nextBtn.click((e)=>{    
+    e.preventDefault();
     moveSlide(currentIdx + 1);      
   });
 
-  prevBtn.click(()=>{      
+  prevBtn.click((e)=>{    
+    e.preventDefault();   
     moveSlide(currentIdx - 1);
   });
 
